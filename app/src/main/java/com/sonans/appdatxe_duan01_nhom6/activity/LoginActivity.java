c6832a814b252e124642f7cbe5e6363d0fe5f8a8
nguyen_phuc <nguyenphuc02042004@gmail.com>
2023-11-13 12:45:23 +0700

    login, signup cua tai xe

--- app/src/main/java/com/sonans/appdatxe_duan01_nhom6/activity/LoginActivity.java
+++ app/src/main/java/com/sonans/appdatxe_duan01_nhom6/activity/LoginActivity.java
@@ -81,7 +81,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(LoginActivity.this, SignUpTaiXeActivity.class);
-                //hrhreh
+                //hrhrehgrger
                 startActivity(i);
             }
         });
