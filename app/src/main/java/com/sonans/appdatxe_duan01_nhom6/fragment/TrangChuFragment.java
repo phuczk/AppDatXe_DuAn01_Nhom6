852571bacc857eb824100ec81acbf90d9b353900
thienhmph39533 <thienhmph39533@fpt.edu.vn>
2023-11-17 12:27:36 +0700

    trang chu khách hang,doi mat khau, tin nhan

---
+++ app/src/main/java/com/sonans/appdatxe_duan01_nhom6/fragment/TrangChuFragment.java
@@ -0,0 +1,29 @@
+package com.sonans.appdatxe_duan01_nhom6.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sonans.appdatxe_duan01_nhom6.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link TrangChuFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TrangChuFragment extends Fragment {
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_trang_chu, container, false);
+    }
+}
